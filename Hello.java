c52eb26798e2a21a781e3e418a2b6bb4d851f270
puneetgavri <puneet.gavri@gmail.com>
2022-08-20 15:57:54 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -7,6 +7,6 @@ public class Hello
     // Prints "Hello, World" to the terminal window.
     public static void main(String args[])
     {
-        System.out.println("Hello, World.how are you");
+        System.out.println("Hey, World.how are you");
     }
 }
