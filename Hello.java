c5a6f6e7193e7da3307ff245dea9226248d7504d
puneetgavri <puneet.gavri@gmail.com>
2022-10-12 06:00:36 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@
 public class Hello
 {
     // Your program begins with a call to main().
-    // Prints "Hello, World" to the terminal window.
+    // "Hello, World" to the terminal window.
     public static void main(String args[])
     {
         System.out.println("Hey,World.how are you");
