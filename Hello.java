ef9bd197b3a88a456c5c98b2b47c6622097b3468
puneetgavri <puneet.gavri@gmail.com>
2022-12-21 22:30:21 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,10 +3,10 @@
  
 public class Hello
 {
-    // Your program begins with a call to main().ndnlnalskmlas
+    // Your program begins with a call to main().
     // "Hello, World" to the terminal window.
     public static void main(String args[])
     {
-        System.out.println("Hey,Puneet.how are you");
+        System.out.println("Hello,John.how are you");
     }
 }
